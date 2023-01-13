1d96261b75acf7ca6211980a7687ceaeb330b9a7
Morgován Nimród <morgovannimrod22@gmail.com>
2023-01-13 10:10:52 +0100

    Visszalépés Main activityre kotlinos activityről

--- app/src/main/java/hu/petrik/kotlinproject/KotlinActivity.kt
+++ app/src/main/java/hu/petrik/kotlinproject/KotlinActivity.kt
@@ -1,11 +1,31 @@
 package hu.petrik.kotlinproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class KotlinActivity : AppCompatActivity() {
+    private lateinit var visszaGomb: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
+        init()
+        addListeners()
     }
+
+    private fun addListeners() {
+        visszaGomb.setOnClickListener { view: View? ->
+            val intent = Intent(this@KotlinActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun init() {
+        visszaGomb = findViewById(R.id.visszaGomb)
+    }
+
 }
