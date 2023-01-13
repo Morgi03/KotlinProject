39a265cc8ca8dc0b72e713cbea8767bb90ced0e0
Morgován Nimród <morgovannimrod22@gmail.com>
2023-01-13 09:44:15 +0100

    Kotlin activity létrehozása

---
+++ app/src/main/java/hu/petrik/kotlinproject/KotlinActivity.kt
@@ -0,0 +1,11 @@
+package hu.petrik.kotlinproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KotlinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+    }
+}
