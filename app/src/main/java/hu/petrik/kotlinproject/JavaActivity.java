acdd714fb0e00e7307d4cdd0ee95ff25776909a8
Morgován Nimród <morgovannimrod22@gmail.com>
2023-01-13 08:47:42 +0100

    Navigáció activityk között

--- app/src/main/java/hu/petrik/kotlinproject/JavaActivity.java
+++ app/src/main/java/hu/petrik/kotlinproject/JavaActivity.java
@@ -2,13 +2,30 @@ package hu.petrik.kotlinproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class JavaActivity extends AppCompatActivity {
-
+    private Button visszaGomb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java);
+        init();
+        addListeners();
+    }
+
+    private void addListeners() {
+        visszaGomb.setOnClickListener(view -> {
+            Intent intent = new Intent(JavaActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+
+    private void init() {
+        visszaGomb = findViewById(R.id.javaGomb);
     }
 }
